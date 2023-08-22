a2d08a41051aa1e0c6d912fb081d33a63b770fd4
rickcollette <rcollet@gmail.com>
2023-08-22 21:00:33 +0000

    fixed processor, added documentation

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# text2md
-converts text files to markdown
+# go2md: Go Documentation to Markdown Converter
+
+go2md is a tool that reads Go source files and generates a corresponding markdown documentation based on GoDoc styled comments present in the code. It supports reading from a single file, from piped input, or recursively from a directory.
+
+## Installation
+Ensure you have Go installed on your system.
+
+Navigate to the directory where go2md resides.
+
+Run the following command to compile the program:
+
+``go
+go build
+```
+
+This will produce an executable named go2md in the current directory.
+
+Usage
+1. Recursively process a directory:
+
+```bash
+./go2md -r /path/to/directory -o /path/to/output.md
+```
+
+This will process all .go files found in the specified directory (and its sub-directories) and generate a markdown documentation in the output file.
+
+2. Process a single file:
+
+```bash
+./go2md -i filename.go -o /path/to/output.md
+```
+
+This will process the specified .go file and generate the markdown documentation in the output file.
+
+3. Process piped input:
+
+```bash
+cat filename.go | ./go2md -o /path/to/output.md
+```
+
+This allows you to pipe the content of a .go file into go2md and get the markdown documentation in the specified output file.
+
+## Documentation Formatting Guide
+In your Go code, to get the best results, follow this commenting style:
+
+```go
+// Title: Your Title Here
+// Description: A brief description of the function or type.
+// Function: The function signature or type definition.
+// CalledWith: How the function can be called (for functions only).
+// ExpectedOutput: What to expect as output (for functions only).
+func YourFunction() {...}
+```
+
+**Title:** A brief title for the function or type.
+**Description:** A more detailed description.
+**Function:** The function signature or type definition.
+**CalledWith:** (For functions) Example(s) of how the function can be called.
+**ExpectedOutput:** (For functions) What the function returns or the expected output.
+
+go2md will parse these comments and generate a structured markdown documentation.
--- main.go
+++ main.go
@@ -1,69 +1,43 @@
-// text2md: A tool to convert GoDoc style documentation to Markdown format.
-//
-// This utility can take input from a piped command, an input file or directly from the terminal.
-// The output can be directed to a file or printed on the terminal.
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-// main is the entry point of the application.
 func main() {
-	// Define and parse command line flags
-	input := flag.String("i", "", "Input file name. Read from the file instead of stdin.")
+	dir := flag.String("r", "", "Directory to scan for .go files.")
+	inputFile := flag.String("i", "", "Input .go file.")
 	output := flag.String("o", "", "Output file name. Write to the file instead of stdout.")
-
 	flag.Parse()
 
-	var reader io.Reader
-
-	// Check if data is being piped in
-	stat, _ := os.Stdin.Stat()
-	if (stat.Mode() & os.ModeCharDevice) == 0 {
-		// Reading from stdin
-		reader = os.Stdin
-	} else if *input != "" {
-		// Reading from the specified input file
-		file, err := os.Open(*input)
-		if err != nil {
-			fmt.Println("Error opening input file:", err)
-			return
-		}
-		defer file.Close()
-		reader = file
-	} else {
-		// No input source provided
-		fmt.Println("No input provided!")
-		return
-	}
-
 	var mdBuilder strings.Builder
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.HasPrefix(line, "    ") {
-			// Convert indented lines to code blocks in Markdown
-			mdBuilder.WriteString("```go\n")
-			mdBuilder.WriteString(strings.TrimPrefix(line, "    ") + "\n")
-			mdBuilder.WriteString("```\n")
-		} else if strings.HasPrefix(line, "TYPE") || strings.HasPrefix(line, "FUNC") {
-			// Convert TYPE or FUNC prefixes to headers in Markdown
-			mdBuilder.WriteString("## " + line + "\n")
-		} else {
-			// Regular lines are preserved as they are
-			mdBuilder.WriteString(line + "\n")
-		}
+
+	if *dir != "" {
+		// Recursively process directory
+		filepath.Walk(*dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if strings.HasSuffix(path, ".go") {
+				processFile(path, &mdBuilder)
+			}
+			return nil
+		})
+	} else if *inputFile != "" {
+		// Process single file
+		processFile(*inputFile, &mdBuilder)
 	}
 
 	// Determine output destination and write the generated markdown content
 	if *output != "" {
-		// Write to the specified output file
 		err := os.WriteFile(*output, []byte(mdBuilder.String()), 0644)
 		if err != nil {
 			fmt.Println("Error writing to output file:", err)
@@ -71,7 +45,84 @@ func main() {
 		}
 		fmt.Printf("Documentation saved to %s!\n", *output)
 	} else {
-		// Print to stdout
 		fmt.Print(mdBuilder.String())
+	}
+}
+
+func processFile(filename string, mdBuilder *strings.Builder) {
+	fileSet := token.NewFileSet()
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading the file:", err)
+		return
+	}
+
+	formattedSource, err := format.Source(content)
+	if err != nil {
+		fmt.Println("Error formatting the source:", err)
+		return
+	}
+
+	node, err := parser.ParseFile(fileSet, filename, formattedSource, parser.ParseComments)
+	if err != nil {
+		fmt.Println("Error parsing Go file:", err)
+		return
+	}
+
+	if node.Doc != nil {
+		mdBuilder.WriteString("# " + node.Name.Name + "\n\n")
+		for _, comment := range node.Doc.List {
+			mdBuilder.WriteString(comment.Text + "\n")
+		}
+		mdBuilder.WriteString("\n")
+	}
+
+	for _, decl := range node.Decls {
+		switch d := decl.(type) {
+		case *ast.GenDecl:
+			processGenDecl(d, mdBuilder)
+		case *ast.FuncDecl:
+			processFuncDecl(d, mdBuilder)
+		}
+	}
+}
+
+func processGenDecl(decl *ast.GenDecl, mdBuilder *strings.Builder) {
+	for _, spec := range decl.Specs {
+		switch s := spec.(type) {
+		case *ast.ValueSpec:
+			for _, name := range s.Names {
+				mdBuilder.WriteString(fmt.Sprintf("## %s (%s)\n\n", name.Name, decl.Tok))
+				if s.Doc != nil {
+					mdBuilder.WriteString(s.Doc.Text() + "\n\n")
+				}
+			}
+		case *ast.TypeSpec:
+			mdBuilder.WriteString(fmt.Sprintf("## Type: %s\n\n", s.Name.Name))
+			if s.Doc != nil {
+				mdBuilder.WriteString(s.Doc.Text() + "\n\n")
+			}
+		}
+	}
+}
+
+func processFuncDecl(decl *ast.FuncDecl, mdBuilder *strings.Builder) {
+	mdBuilder.WriteString(fmt.Sprintf("## Function: %s\n\n", decl.Name.Name))
+	if decl.Doc != nil {
+		var title, description, function, calledWith, expectedOutput string
+		for _, comment := range decl.Doc.List {
+			if strings.HasPrefix(comment.Text, "// Title:") {
+				title = strings.TrimPrefix(comment.Text, "// Title:")
+			} else if strings.HasPrefix(comment.Text, "// Description:") {
+				description = strings.TrimPrefix(comment.Text, "// Description:")
+			} else if strings.HasPrefix(comment.Text, "// Function:") {
+				function = strings.TrimPrefix(comment.Text, "// Function:")
+			} else if strings.HasPrefix(comment.Text, "// CalledWith:") {
+				calledWith = strings.TrimPrefix(comment.Text, "// CalledWith:")
+			} else if strings.HasPrefix(comment.Text, "// ExpectedOutput:") {
+				expectedOutput = strings.TrimPrefix(comment.Text, "// ExpectedOutput:")
+			}
+		}
+		mdBuilder.WriteString(fmt.Sprintf("**Title:** %s\n\n**Description:** %s\n\n**Function:** %s\n\n**Called With:** %s\n\n**Expected Output:** %s\n\n", title, description, function, calledWith, expectedOutput))
 	}
 }
