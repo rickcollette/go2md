a9f141d268ff78fcf42759c941913baf545456ae
rickcollette <rcollet@gmail.com>
2023-08-22 21:44:28 +0000

    fixed more formatting

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# go2md: Go Documentation to Markdown Converter
+# go2md
+## Go Source to Markdown Documentation Creator
 
-go2md is a tool that reads Go source files and generates a corresponding markdown documentation based on GoDoc styled comments present in the code. It supports reading from a single file, from piped input, or recursively from a directory.
+go2md is a tool that reads Go source files and generates a corresponding markdown documentation based on go2md styled comments present in the code. It supports reading from a single file, from piped input, or recursively from a directory.
 
 ## Installation
 Ensure you have Go installed on your system.
@@ -9,8 +10,8 @@ Navigate to the directory where go2md resides.
 
 Run the following command to compile the program:
 
-``go
-go build
+```go
+go build -o go2md main.go
 ```
 
 This will produce an executable named go2md in the current directory.
@@ -43,19 +44,102 @@ This allows you to pipe the content of a .go file into go2md and get the markdow
 ## Documentation Formatting Guide
 In your Go code, to get the best results, follow this commenting style:
 
+For the top of the file:
+
+```go
+// Package: main
+// Description: Description of the package and its purpose.
+// Git Repository: URL of the git repository.
+// License: License type of the code.
+```
+
+For functions and types:
+
 ```go
 // Title: Your Title Here
 // Description: A brief description of the function or type.
 // Function: The function signature or type definition.
 // CalledWith: How the function can be called (for functions only).
 // ExpectedOutput: What to expect as output (for functions only).
+// Example: An example of how to use the function or type.
 func YourFunction() {...}
 ```
 
-**Title:** A brief title for the function or type.
-**Description:** A more detailed description.
-**Function:** The function signature or type definition.
-**CalledWith:** (For functions) Example(s) of how the function can be called.
-**ExpectedOutput:** (For functions) What the function returns or the expected output.
+* **Title:** A brief title for the function or type.
+* **Description:** A more detailed description.
+* **Function:** The function signature or type definition.
+* **CalledWith:** (For functions) Example(s) of how the function can be called.
+* **ExpectedOutput:** (For functions) What the function returns or the expected output.
+* **Example:** (For functions and types) An example showcasing the usage.
+
 
 go2md will parse these comments and generate a structured markdown documentation.
+
+-------------------
+
+## Generated with go2md:
+
+# Package:  main
+
+**Description:**  converts go code into documentation
+
+**Git Repository:**  https://github.com/rickcollette/go2md
+
+**License:**  MIT License
+
+## Function: main
+
+**Title:**  main
+
+**Description:**  Entry point of the program. Converts Go source documentation to Markdown format based on provided flags.
+
+**Function:**  main
+
+**Called With:**  N/A
+
+**Example:**  N/A (Since it's the main function)
+
+**Expected Output:**  Writes markdown content to a file or stdout.
+
+## Function: processFile
+
+**Title:**  processFile
+
+**Description:**  Process a Go file and generate markdown documentation.
+
+**Function:**  processFile
+
+**Called With:**  filename string, mdBuilder *strings.Builder
+
+**Example:**  processFile("example.go", &mdBuilder)
+
+**Expected Output:**  Markdown documentation for the Go file.
+
+## Function: processGenDecl
+
+**Title:**  processGenDecl
+
+**Description:**  Process general declarations (like variables and types) and generates markdown documentation.
+
+**Function:**  processGenDecl
+
+**Called With:**  decl *ast.GenDecl, mdBuilder *strings.Builder
+
+**Example:**  processGenDecl(someGenDecl, &mdBuilder)
+
+**Expected Output:**  Markdown documentation for general declarations.
+
+## Function: processFuncDecl
+
+**Title:**  processFuncDecl
+
+**Description:**  Process function declarations and generates markdown documentation based on specific comment patterns.
+
+**Function:**  processFuncDecl
+
+**Called With:**  decl *ast.FuncDecl, mdBuilder *strings.Builder
+
+**Example:**  processFuncDecl(someFuncDecl, &mdBuilder)
+
+**Expected Output:**  Markdown documentation for function declarations.
+
--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Package: main
+// Description: converts go code into documentation
+// Git Repository: https://github.com/rickcollette/go2md
+// License: MIT License
 package main
 
 import (
@@ -12,6 +16,12 @@ import (
 	"strings"
 )
 
+// Title: main
+// Description: Entry point of the program. Converts Go source documentation to Markdown format based on provided flags.
+// Function: main
+// CalledWith: N/A
+// ExpectedOutput: Writes markdown content to a file or stdout.
+// Example: N/A (Since it's the main function)
 func main() {
 	dir := flag.String("r", "", "Directory to scan for .go files.")
 	inputFile := flag.String("i", "", "Input .go file.")
@@ -49,44 +59,70 @@ func main() {
 	}
 }
 
+// Title: processFile
+// Description: Process a Go file and generate markdown documentation.
+// Function: processFile
+// CalledWith: filename string, mdBuilder *strings.Builder
+// ExpectedOutput: Markdown documentation for the Go file.
+// Example: processFile("example.go", &mdBuilder)
 func processFile(filename string, mdBuilder *strings.Builder) {
-	fileSet := token.NewFileSet()
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Println("Error reading the file:", err)
-		return
-	}
+    fileSet := token.NewFileSet()
+    content, err := os.ReadFile(filename)
+    if err != nil {
+        fmt.Println("Error reading the file:", err)
+        return
+    }
 
-	formattedSource, err := format.Source(content)
-	if err != nil {
-		fmt.Println("Error formatting the source:", err)
-		return
-	}
+    formattedSource, err := format.Source(content)
+    if err != nil {
+        fmt.Println("Error formatting the source:", err)
+        return
+    }
 
-	node, err := parser.ParseFile(fileSet, filename, formattedSource, parser.ParseComments)
-	if err != nil {
-		fmt.Println("Error parsing Go file:", err)
-		return
-	}
+    node, err := parser.ParseFile(fileSet, filename, formattedSource, parser.ParseComments)
+    if err != nil {
+        fmt.Println("Error parsing Go file:", err)
+        return
+    }
 
-	if node.Doc != nil {
-		mdBuilder.WriteString("# " + node.Name.Name + "\n\n")
-		for _, comment := range node.Doc.List {
-			mdBuilder.WriteString(comment.Text + "\n")
-		}
-		mdBuilder.WriteString("\n")
-	}
+    // Process top-of-file package-level documentation
+    if node.Doc != nil {
+        var packageName, description, gitRepo, license string
+        for _, comment := range node.Doc.List {
+            if strings.HasPrefix(comment.Text, "// Package:") {
+                packageName = strings.TrimPrefix(comment.Text, "// Package:")
+            } else if strings.HasPrefix(comment.Text, "// Description:") {
+                description = strings.TrimPrefix(comment.Text, "// Description:")
+            } else if strings.HasPrefix(comment.Text, "// Git Repository:") {
+                gitRepo = strings.TrimPrefix(comment.Text, "// Git Repository:")
+            } else if strings.HasPrefix(comment.Text, "// License:") {
+                license = strings.TrimPrefix(comment.Text, "// License:")
+            } else {
+                // For other comments that don't match the specific tags, append them as-is
+                mdBuilder.WriteString(comment.Text + "\n")
+            }
+        }
+        mdBuilder.WriteString(fmt.Sprintf("# Package: %s\n\n**Description:** %s\n\n**Git Repository:** %s\n\n**License:** %s\n\n",
+                                          packageName, description, gitRepo, license))
+    }
 
-	for _, decl := range node.Decls {
-		switch d := decl.(type) {
-		case *ast.GenDecl:
-			processGenDecl(d, mdBuilder)
-		case *ast.FuncDecl:
-			processFuncDecl(d, mdBuilder)
-		}
-	}
+    for _, decl := range node.Decls {
+        switch d := decl.(type) {
+        case *ast.GenDecl:
+            processGenDecl(d, mdBuilder)
+        case *ast.FuncDecl:
+            processFuncDecl(d, mdBuilder)
+        }
+    }
 }
 
+
+// Title: processGenDecl
+// Description: Process general declarations (like variables and types) and generates markdown documentation.
+// Function: processGenDecl
+// CalledWith: decl *ast.GenDecl, mdBuilder *strings.Builder
+// ExpectedOutput: Markdown documentation for general declarations.
+// Example: processGenDecl(someGenDecl, &mdBuilder)
 func processGenDecl(decl *ast.GenDecl, mdBuilder *strings.Builder) {
 	for _, spec := range decl.Specs {
 		switch s := spec.(type) {
@@ -106,10 +142,16 @@ func processGenDecl(decl *ast.GenDecl, mdBuilder *strings.Builder) {
 	}
 }
 
+// Title: processFuncDecl
+// Description: Process function declarations and generates markdown documentation based on specific comment patterns.
+// Function: processFuncDecl
+// CalledWith: decl *ast.FuncDecl, mdBuilder *strings.Builder
+// ExpectedOutput: Markdown documentation for function declarations.
+// Example: processFuncDecl(someFuncDecl, &mdBuilder)
 func processFuncDecl(decl *ast.FuncDecl, mdBuilder *strings.Builder) {
 	mdBuilder.WriteString(fmt.Sprintf("## Function: %s\n\n", decl.Name.Name))
 	if decl.Doc != nil {
-		var title, description, function, calledWith, expectedOutput string
+		var title, description, function, calledWith, example, expectedOutput string
 		for _, comment := range decl.Doc.List {
 			if strings.HasPrefix(comment.Text, "// Title:") {
 				title = strings.TrimPrefix(comment.Text, "// Title:")
@@ -119,10 +161,12 @@ func processFuncDecl(decl *ast.FuncDecl, mdBuilder *strings.Builder) {
 				function = strings.TrimPrefix(comment.Text, "// Function:")
 			} else if strings.HasPrefix(comment.Text, "// CalledWith:") {
 				calledWith = strings.TrimPrefix(comment.Text, "// CalledWith:")
+			} else if strings.HasPrefix(comment.Text, "// Example:") {
+				example = strings.TrimPrefix(comment.Text, "// Example:")
 			} else if strings.HasPrefix(comment.Text, "// ExpectedOutput:") {
 				expectedOutput = strings.TrimPrefix(comment.Text, "// ExpectedOutput:")
 			}
 		}
-		mdBuilder.WriteString(fmt.Sprintf("**Title:** %s\n\n**Description:** %s\n\n**Function:** %s\n\n**Called With:** %s\n\n**Expected Output:** %s\n\n", title, description, function, calledWith, expectedOutput))
+		mdBuilder.WriteString(fmt.Sprintf("**Title:** %s\n\n**Description:** %s\n\n**Function:** %s\n\n**Called With:** %s\n\n**Example:** %s\n\n**Expected Output:** %s\n\n", title, description, function, calledWith, example, expectedOutput))
 	}
 }
